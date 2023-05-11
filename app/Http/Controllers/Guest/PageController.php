1bd8bf7f4bb1e4601322aa7c58886737d2fcc4ba
christianarca27 <christian.arca.mail@gmail.com>
2023-05-11 16:36:23 +0200

    feat: visualizzati SOLO i treni con orario di partenza successivo al momento in cui si apre l'applicazione

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use DateTime;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $dateTime = new DateTime();
+
+        $trains = Train::where('departure_time', '>=', $dateTime->format('Y-m-d H:i:s'))->get();
 
         return view('index', compact('trains'));
     }
