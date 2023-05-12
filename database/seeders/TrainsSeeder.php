52957c6a6ac8fdc7e698cce977b37314d691a183
christianarca27 <christian.arca.mail@gmail.com>
2023-05-12 16:42:09 +0200

    feat: modificata generazione data di partenza e arrivo per renderle congrue ad un possibile viaggio

--- database/seeders/TrainsSeeder.php
+++ database/seeders/TrainsSeeder.php
@@ -23,8 +23,8 @@ class TrainsSeeder extends Seeder
             $newTrain->company = $generator->company();
             $newTrain->departure_station = $generator->city();
             $newTrain->arrival_station = $generator->city();
-            $newTrain->departure_time = $generator->dateTime();
-            $newTrain->arrival_time = $generator->dateTime();
+            $newTrain->departure_time = $generator->dateTimeBetween('-1 month', '+1 month');
+            $newTrain->arrival_time = $generator->dateTimeInInterval($newTrain->departure_time, '+12 hours');
             $newTrain->train_code = $generator->regexify('[A-Z]{2}[0-9]{3}[A-Z]{2}');
             $newTrain->carriages_number = $generator->numberBetween(5, 20);
             $newTrain->is_on_time = $generator->numberBetween(0, 1);
